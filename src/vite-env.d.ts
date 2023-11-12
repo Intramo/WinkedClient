7b973abb8b19197fa6c17f45eb74103ce2ec32c2
Fabian F <78906517+FabulousCodingFox@users.noreply.github.com>
2023-11-12 18:41:18 +0100

    Toasts & QOL

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,3 +2,6 @@
 /// <reference types="vite/client" />
 
 type ColorScheme = "light" | "dark" | "pink";
+
+type ToastType = "error" | "info";
+type Toast = { message: string; duration: number; type: ToastType };
